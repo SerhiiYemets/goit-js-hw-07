42bc0d324f7676dde672193528a8c00e6ec7a389
Serhii Yemets <Serhiiyemets2020@gmail.com>
2025-09-10 11:15:42 +0200

    add js file

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,11 @@
 const listItem = document.querySelectorAll('li.item');
-console.log(`Кількість категорій: ${listItem.length}`);
+console.log(`Number of categories: ${listItem.length}`);
 
 listItem.forEach(function(item) {
     const title = item.querySelector('h2').textContent; 
     const count = item.querySelectorAll('li').length;   
-    console.log(`Категорія: ${title}, Кількість елементів: ${count}`);
+    console.log(`Category: ${title}`)
+    console.log(`Elements: ${count}`);
 });
 
 
